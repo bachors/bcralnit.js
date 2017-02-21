274dbc43c7646db7d7d49b6cc7d601f65dc366ac
Ican Bachors <bachor.can@gmail.com>
2017-02-21 22:11:12 +0700

    textarea, pre, div etc

--- js/bcralnit.js
+++ js/bcralnit.js
@@ -10,17 +10,20 @@ $.fn.bcralnit = function(e) {
     var f = {
         width: '60px',
         background: '#ddd',
-        color: '#333'
+        color: '#333',
+        addClass: ''
     };
     if (typeof e === 'object') {
         e.width = (e.width == undefined ? f.width : e.width);
         e.background = (e.background == undefined ? f.background : e.background);
-        e.color = (e.color == undefined ? f.color : e.color)
+        e.color = (e.color == undefined ? f.color : e.color);
+        e.addClass = (e.addClass == undefined ? f.addClass : e.addClass)
     } else {
         e = f
     }
     $(this).each(function(i, a) {
-        var f = $(this)[0].tagName,
+        var f = $(this)[0].tagName,		
+			kk = (f == 'TEXTAREA' ? $(this).val() : $(this).text()),
             w = $(this).css('width'),
             h = $(this).css('height'),
             d = ($(this).css('display') == 'inline' ? 'inline-block' : $(this).css('display')),
@@ -39,7 +42,10 @@ $.fn.bcralnit = function(e) {
             pr = $(this).css('padding-right'),
             nw = (parseInt(w) - parseInt(e.width)) + 'px',
             c = ($(this).attr('id') != null && $(this).attr('id') != undefined ? 'bcralnit_' + $(this).attr('id') + i : 'bcralnit_' + $(this).attr('class') + i),
-            b = '<div class="bcr_number" style="text-shadow:none;position:absolute;box-sizing:border-box;border:none;margin:0px;overflow-x:hidden;overflow-y:auto;white-space:pre-wrap;white-space:-moz-pre-wrap;white-space:-pre-wrap;white-space:-o-pre-wrap;word-wrap:break-word;padding:' + (parseInt(pt) + parseInt(bt)) + 'px ' + pr + ' ' + pb + ' ' + pl + ';font-family:' + ff + ';font-size:' + fs + ';font-weight:' + fw + ';line-height:' + lh + ';right:' + e.width + ';width:' + (parseInt(nw) - parseInt(br) - parseInt(bl)) + 'px;height:calc(100% - ' + bb + ');background:' + e.background + ';color:' + e.background + '"></div>';
+            b = '<div class="bcr_number" style="text-shadow:none;position:absolute;box-sizing:border-box;border:none;margin:0px;overflow-x:hidden;overflow-y:auto;white-space:pre-wrap;white-space:-moz-pre-wrap;white-space:-pre-wrap;white-space:-o-pre-wrap;word-wrap:break-word;padding:' + (parseInt(pt) + parseInt(bt)) + 'px ' + pr + ' ' + pb + ' ' + pl + ';font-family:' + ff + ';font-size:' + fs + ';font-weight:' + fw + ';line-height:' + lh + ';right:' + e.width + ';width:' + (parseInt(nw) - parseInt(br) - parseInt(bl)) + 'px;height:calc(100% - ' + bb + ');background:' + e.background + ';color:' + e.background + '"></div>';	
+		if(e.addClass != ''){
+			$(this).addClass(e.addClass);
+		}
         $(this).css('width', nw);
         $(this).css('height', '100%');
         $(this).css('position', 'absolute');
@@ -59,26 +65,28 @@ $.fn.bcralnit = function(e) {
         $(this).addClass("bcr_line");
         $(this).wrap('<div id="' + c + '" style="display:' + d + ';overflow:hidden;box-sizing:border-box;border:none;background:' + e.background + ';position:relative;padding:0px;margin:' + m + ';width:' + w + ';height:' + h + '"></div>');
         $(this).before(b);
-        var b = (f == 'TEXTAREA' ? $(this).val().split('\n').length : $(this).text().split(/\n/).length);
-        addln(f, b, c);
+        addln(c, kk);
         $(this).on('blur focus change keyup keydown', function() {
-            var a = (f == 'TEXTAREA' ? $(this).val().split('\n').length : $(this).text().split(/\n/).length);
-            addln(f, a, c)
+			if(f == 'TEXTAREA'){
+				var vv = $(this).val();
+				addln(c, vv);
+			}
         });
         $(this).scroll(function() {
             $('#' + c + ' .bcr_number').scrollTop($(this).scrollTop())
         })
     });
 
-    function addln(d, a, b) {
-        var c = '';
-        for (i = 0; i < a; i++) {
+    function addln(b, vv) {
+        var a = vv.split(/\n/),
+			c = '';
+        for (i = 0; i < a.length; i++) {
             var n = (i + 1);
-            var f = (d == 'TEXTAREA' ? $('#' + b + ' .bcr_line').val().split('\n')[i] : $('#' + b + ' .bcr_line').text().split(/\n/)[i]);
+            var f = a[i];
             var g = f.substring(n.toString().length, f.length);
             c += '<span style="background:transparent;border:none;box-shadow:none;color:' + e.color + '">' + n + '</span>' + g.replace(/\</ig, '~').replace(/\>/ig, '~') + '<br>'
         };
         $('#' + b + ' .bcr_number').html(c);
-        $('#' + b + ' .bcr_number').scrollTop($('#' + b + ' .bcr_line').scrollTop())
+        $('#' + b + ' .bcr_number').scrollTop($('#' + b + ' .bcr_line').scrollTop());
     }
 }
